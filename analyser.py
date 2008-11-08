e86ab640e608ef89dac4d7f78043635d21edfc49
Fabs <fabriciosn@gmail.com>
2008-11-08 17:51:50 -0200

    python-twitter

--- analyser.py
+++ analyser.py
@@ -2,6 +2,7 @@ from urllib import urlopen
 
 def extractKeyWords(context,query=""):
    context = context.replace(" ","+")
+   query = query.replace(" ","+")
    urlbase = "http://search.yahooapis.com/ContentAnalysisService/V1/termExtraction?appid=EventOMeeter&output=json&context=%s&query=%s"%(context,query)
    results = urlopen(urlbase)
    jsonResults = results.read()
@@ -9,4 +10,5 @@ def extractKeyWords(context,query=""):
    return dic['ResultSet']['Result']
    
 
-print extractKeyWords("Italian sculptors and painters of the renaissance favored the Virgin Mary for inspiration")
+if __name__ == '__main__':
+   print extractKeyWords(raw_input("Enter Text\n"),"cold war")
