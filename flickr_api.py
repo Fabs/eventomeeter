2de4c56bea68cbc470850e6bf9b075ad77a7fa74
Fabs <fabriciosn@gmail.com>
2008-11-08 18:56:54 -0200

    Flickr api agora é JSON

--- flickr_api.py
+++ flickr_api.py
@@ -1,20 +1,19 @@
 import flickrapi
 from flickr_keys import *
-from xml.etree import ElementTree
 import sys
 
-def auth_flickr(format='etree'):
+def authFlickr(format='etree'):
     """login no flickr"""
     f = flickrapi.FlickrAPI(webapi,secret=websecret,format=format)
     return f
 
-def get_recents(xml='etree'):
+def getFlickers(xml='etree'):
     """retorna o XML das fotos"""
-    flickr = auth_flickr()
-    recentes = flickr.photos_getrecent(user_id=userid, per_page='100',format=xml)
-    return recentes
+    flickr = authFlickr()
+    recent = flickr.photos_getrecent(user_id=userid, per_page='1',format='json')
+    return recent
 
     
 if __name__=='__main__':
-    get_recents()
-    
+    recent = getFlickers()
+    print recent
