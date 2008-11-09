0ae8441adc64fcd585ca943b7612b1bf881188eb
Rodrigo Bernardo Pimentel <rbp@locutus.(none)>
2008-11-09 09:09:35 -0200

    Twitter API 2

---
+++ twitter_api2.py
@@ -0,0 +1,19 @@
+from urllib import urlopen
+
+url = 'http://search.twitter.com/search.json?q=%s'
+
+def query_twitter(term):
+    s = urlopen(url % (term)).read()
+    dic = eval(s.replace('null', 'None'))
+    #print dic
+    return dic
+
+if __name__ == '__main__':
+    d = {}
+    for post in query_twitter("brhackday")['results']:
+        try:
+            d[post['from_user']] += 1
+        except KeyError:
+            d[post['from_user']] = 1
+
+    print d
